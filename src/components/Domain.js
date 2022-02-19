3d5e39823081ffc76523d42d950d5c598d80c0e4
yhx <yhx15779081105@163.com>
2022-02-19 22:41:40 +0800

    docs：同步本地修改

--- src/components/Domain.js
+++ src/components/Domain.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Test from './Test'
+import { debounce, throttle } from "lodash";
+
+import Test from "./Test";
 
 class Domain extends Component {
-  componentDidMount(){
-    console.log(this.props)
+  componentDidMount() {
+    console.log(this.props);
   }
   render() {
     return (
       <div>
         domain
         <Test />
+        <div>
+          <button
+            onClick={debounce(() => {
+              console.log(122222);
+            }, 2000)}
+          >
+            aaasd
+          </button>
+        </div>
       </div>
     );
   }
